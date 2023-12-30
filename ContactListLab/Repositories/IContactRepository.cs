dcbada4996feec036aaf1fdca4ec038e754355c1
vlntny <bolotovvvva@gmail.com>
2023-12-31 01:46:46 +0300

    Web

---
+++ ContactListLab/Repositories/IContactRepository.cs
@@ -0,0 +1,11 @@
+﻿using ContactListLab.Model;
+using ContactListLab.Presenter;
+
+namespace ContactListLab.Repositories;
+
+public interface IContactRepository
+{
+    Task<List<ContactDto>> ViewAllContacts();
+    Task<List<ContactDto>> SearchContacts(int search, string searchString);
+    Task AddContact(ContactDto contact);
+}
